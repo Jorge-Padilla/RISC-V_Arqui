0e5ff6781b6fefe9e2f66589199537ddcc0974bf
Jorge Padilla <a01635346@itesm.mx>
2023-03-20 00:16:48 -0600

    Practica 1 ended and ready to be shown

--- quartus/db/ip/sld89fdeded/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ quartus/db/ip/sld89fdeded/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 917aa2f38136cecd31f5
+//   DESIGN_HASH   : f8fa9052351d5a64e424
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h917aa2f38136cecd31f5 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf8fa9052351d5a64e424 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
